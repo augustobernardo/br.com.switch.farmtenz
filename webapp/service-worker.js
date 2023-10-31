9d8738c3e6b4f6fc81ff10f8d684163cf1cbb565
Augusto Oliveira <gutooliveira027@gmail.com>
2023-10-31 16:32:36 -0300

    Carga Inicial

---
+++ webapp/service-worker.js
@@ -0,0 +1,20 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('my-cache').then(function(cache) {
+      return cache.addAll([
+        '/index.html',
+        '/Component.js',
+		'/manifest.json'
+        // Adicione aqui os recursos que deseja que sejam armazenados em cache
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
